2856ef073deced8b990514f95c3ebb4c41b8edb0
akilagithub <30523620+akilagithub@users.noreply.github.com>
2022-07-19 16:54:43 +0530

    Update eks_variables.tf

--- infrastructure/stage/variables/eks_variables.tf
+++ infrastructure/stage/variables/eks_variables.tf
@@ -14,8 +14,8 @@
  * limitations under the License.
  */
 
-variable "eks1_cluster_name" { default = "eks-stage-us-east1ab-1" }
-variable "eks2_cluster_name" { default = "eks-stage-us-east1ab-2" }
+variable "eks1_cluster_name" { default = "eks-stage-eu-west1ab-1" }
+variable "eks2_cluster_name" { default = "eks-stage-eu-west1ab-2" }
 
 variable "config-repo" { default = "config" }
 variable "env" { default = "stage" }
